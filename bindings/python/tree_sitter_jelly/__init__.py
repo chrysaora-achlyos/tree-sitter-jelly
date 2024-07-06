8ec55cb2ccb11a02ae3e94261fcb584eb9a5306b
dev <mbpro.m1.dev@ma7.org>
2024-07-06 17:28:43 -0500

    see this in tree-sitter-lua and tree-sitter-ruby, so going to add

---
+++ bindings/python/tree_sitter_jelly/__init__.py
@@ -0,0 +1,5 @@
+"Jelly grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/swift/TreeSitterJelly/jelly.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_JELLY_H_
+#define TREE_SITTER_JELLY_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_jelly(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_JELLY_H_
